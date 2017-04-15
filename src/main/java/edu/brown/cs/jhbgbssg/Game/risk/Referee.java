000f986d5a37af577a602921caf55e3be96a54df
Sarah Gilmore <sarahgilmore@sarahs-mbp-2.devices.brown.edu>
2017-04-15 18:30:52 -0400

    referee

--- src/main/java/edu/brown/cs/jhbgbssg/Game/risk/Referee.java
+++ src/main/java/edu/brown/cs/jhbgbssg/Game/risk/Referee.java
@@ -3,6 +3,7 @@ package edu.brown.cs.jhbgbssg.Game.risk;
 import java.util.UUID;
 
 import edu.brown.cs.jhbgbssg.RiskWorld.Territory;
+import edu.brown.cs.jhbgbssg.RiskWorld.TerritoryEnum;
 
 /**
  * Controls the rules of the game.
@@ -17,6 +18,40 @@ public class Referee {
    */
   public Referee() {
 
+  }
+
+  public static boolean checkPlayerTurn(Turn turn, UUID playerID) {
+    return turn.getPlayerId().equals(playerID);
+  }
+
+  public static boolean checkAttackPhase(Turn turn) {
+    return turn.getAttacking();
+  }
+
+  public static boolean checkAdjacentTerritory(RiskBoard board,
+      TerritoryEnum terr1, TerritoryEnum terr2) {
+    return board.isNeighbor(terr1, terr2);
+  }
+
+  public static boolean checkTerritoryOwner(RiskPlayer player,
+      TerritoryEnum terr) {
+    return player.hasTerritory(terr);
+  }
+
+  public static boolean checkTerritoryAttack(Territory terr) {
+    return terr.getNumberTroops() > 1;
+  }
+
+  public static boolean checkNumberDieAttack(Territory terr, int numberDie) {
+    return terr.getNumberTroops() > numberDie;
+  }
+
+  public static boolean checkNumberDieDefend(Territory terr, int numberDie) {
+    return terr.getNumberTroops() >= numberDie;
+  }
+
+  public static boolean checkMovementNumber(Territory terr, int numberMove) {
+    return terr.getNumberTroops() > numberMove;
   }
 
   /**
