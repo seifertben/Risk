3c94dfe75ef3e7b122eadf7ec6f06909ce5137e9
Justin Huang <Justin Huang>
2017-04-16 16:48:25 -0400

    gui territories show up

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,22 +1,4 @@
-var icon= "M21.25,8.375V28h6.5V8.375H21.25zM12.25,28h6.5V4.125h-6.5V28zM3.25,28h6.5V12.625h-6.5V28z";
-AmCharts.makeChart( "mapdiv", {
-  /**
-   * this tells amCharts it's a map
-   */
-  "type": "map",
-  "mouseWheelZoomEnabled": true,
-  /**
-   * create data provider object
-   * map property is usually the same as the name of the map file.
-   * getAreasFromMap indicates that amMap should read all the areas available
-   * in the map data and treat them as they are included in your data provider.
-   * in case you don't set it to true, all the areas except listed in data
-   * provider will be treated as unlisted.
-   */
-
-  "dataProvider": {
-    "map": "continentsLow",
-     images: [ {
+let game = [ {
       "latitude": 38.392017, 
       "longitude": -85.047551,
       // "svgPath": icon,
@@ -47,10 +29,10 @@ AmCharts.makeChart( "mapdiv", {
       "type": "circle" ,
       "color": "#CC0000",
       "scale": 0.5,
-      "label": "Quebec",
+      "label": "E. Canada",
       "labelShiftY": 2,
       "zoomLevel": 5,
-      "title": "Quebec",
+      "title": "Eastern Canada",
       "description": "Occupied by Player 1. Soldiers: 5"
     },
      
@@ -65,8 +47,496 @@ AmCharts.makeChart( "mapdiv", {
       "zoomLevel": 5,
       "title": "Ontario",
       "description": "Occupied by Player 1. Soldiers: 5"
-    }
-     ],
+    },
+     {
+      "latitude": 55.279171, 
+      "longitude":  -113.971396 ,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Alberta",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Alberta",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+     {
+      "latitude": 65.199589,
+      "longitude":   -119.253051,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "NW Territories",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Northwest Territories",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+    
+    {
+      "latitude": 65.903817,
+      "longitude":   -154.899950,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Alaska",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Alaska",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+        {
+      "latitude": 74.663742,
+      "longitude":    -41.234447,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Greenland",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Greenland",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+    {
+      "latitude": 22.525567,
+      "longitude":     -102.054754,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "C. America",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Central America",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+  {
+      "latitude": 1.683763, 
+      "longitude":    -73.050849,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Venezuela",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Venezuela",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+     {
+      "latitude": -15.185794,
+      "longitude":    -69.359442,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Peru",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Peru",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+      {
+      "latitude": -11.423151,
+      "longitude":    -51.781319,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Brazil",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Brazil",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+     {
+      "latitude": -32.756957,
+      "longitude":  -65.492254,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Argentina",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Argentina",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+     {
+      "latitude": 64.821171, 
+      "longitude":  -18.807248,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Iceland",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Iceland",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+     {
+      "latitude":   54.047766,
+      "longitude":  -1.398164,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "GB",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Great Britain",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+    {
+      "latitude":    43.295002,
+      "longitude":  -0.071574,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "W EU",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Western Europe",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+    {
+      "latitude":    53.111401,
+      "longitude":   15.536295,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "N EU",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Northern Europe",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    }, 
+      {
+      "latitude":    44.646422,
+      "longitude":   21.718624,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "S EU",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Southern Europe",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+    {
+      "latitude":    62.869291, 
+      "longitude":   14.505908,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Scandinavia",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Scandinavia",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+     {
+      "latitude":    56.551044,
+      "longitude":   39.372611,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Russia",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Russia",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },  
+     {
+      "latitude":    21.054596,
+      "longitude":   0.659746,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "N. AF",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Northern Africa",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+    {
+      "latitude":    21.054596,
+      "longitude":   0.659746,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "N. AF",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Northern Africa",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    }, 
+      {
+      "latitude":   26.520551,
+      "longitude":   23.687088,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Egypt",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Egypt",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+     {
+      "latitude":     -0.075992,
+      "longitude":    19.292558,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "C. AF",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Central Africa",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+    {
+      "latitude":     -0.954854, 
+      "longitude":    36.519119,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "E. AF",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Eastern Africa",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    }, 
+     {
+      "latitude":    -17.883791,
+      "longitude":    22.456620,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "S. AF",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Southern Africa",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+    {
+      "latitude":   -19.073939,
+      "longitude":     46.428030,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Madagascar",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Madagascar",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    }, 
+     {
+      "latitude":   -24.755328,
+      "longitude":     123.346503,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "W. AU",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Western Australia",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+    {
+      "latitude":   -25.384764, 
+      "longitude":     139.468350,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "E. AU",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Eastern Australia",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+     {
+      "latitude":  1.954392, 
+      "longitude":    113.200858,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Indonesia",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Indonesia",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    }, 
+     {
+      "latitude":  -4.849519,
+      "longitude":   140.997146,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "New Guinea",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "New Guinea",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    }, 
+    {
+      "latitude":  27.610497, 
+      "longitude":  43.067386,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Middle East",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Middle East",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },  
+     {
+      "latitude":  21.539913,
+      "longitude":  77.344727,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "India",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "India",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+    
+         {
+      "latitude": 17.900297, 
+      "longitude":  101.426756,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "SE Asia",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "SouthEast Asia",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    }, 
+    {
+      "latitude": 36.897568,
+      "longitude":  103.375284,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "China",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "China",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },  
+    {
+      "latitude": 49.846997,
+      "longitude":  124.554928,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Mongolia",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Mongolia",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },  
+     {
+      "latitude": 37.222275,
+      "longitude":  139.220748,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Japan",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Japan",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    }, 
+     {
+      "latitude": 66.569164,
+      "longitude":   153.670575,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Kamchatka",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Kamchatka",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    }, 
+     {
+      "latitude": 67.092513,
+      "longitude":  131.116421,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Yakutsk",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Yakutsk",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+    {
+      "latitude": 58.664724, 
+      "longitude":  127.776577,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Irkustk",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Irkustk",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    }, 
+     {
+      "latitude": 67.160839,
+      "longitude":  103.342985,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Siberia",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Siberia",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    }, 
+      {
+      "latitude": 66.666218,
+      "longitude":  67.958187,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Ural",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Ural",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    },
+     {
+      "latitude": 37.768380,
+      "longitude": 66.896086,
+      "type": "circle" ,
+      "color": "#CC0000",
+      "scale": 0.5,
+      "label": "Afghanistan",
+      "labelShiftY": 2,
+      "zoomLevel": 5,
+      "title": "Afghanistan",
+      "description": "Occupied by Player 1. Soldiers: 5"
+    } ];
+var icon= "M21.25,8.375V28h6.5V8.375H21.25zM12.25,28h6.5V4.125h-6.5V28zM3.25,28h6.5V12.625h-6.5V28z";
+AmCharts.makeChart( "mapdiv", {
+  /**
+   * this tells amCharts it's a map
+   */
+  "type": "map",
+  "mouseWheelZoomEnabled": true,
+  /**
+   * create data provider object
+   * map property is usually the same as the name of the map file.
+   * getAreasFromMap indicates that amMap should read all the areas available
+   * in the map data and treat them as they are included in your data provider.
+   * in case you don't set it to true, all the areas except listed in data
+   * provider will be treated as unlisted.
+   */
+
+  "dataProvider": {
+    "map": "continentsLow",
+     images:game,
     "areas": [
       {
         "id": "europe",
