1460b090c20517e08bb8b8fa33c0c360f18dd4b3
Sarah Gilmore <sarahgilmore@sarahs-mbp-2.devices.brown.edu>
2017-04-25 16:23:53 -0400

    added RiskMessage enum and switched to ordinals for terrs

---
+++ src/main/java/edu/brown/cs/jhbgbssg/Game/risk/RiskMessageType.java
@@ -0,0 +1,7 @@
+package edu.brown.cs.jhbgbssg.Game.risk;
+
+public enum RiskMessageType {
+
+  CONNECT, REMOVE, START, JOIN, CREATE, CHANGE, DESTROY, WINNER, LOSER, HANDOUT_CARD, NO_CARDS_LEFT, PREVIOUS_ACTION, VALID_ACTIONS, ERROR;
+
+}
