bd3fc7e7ea5b4cd35860c93576c1fda946f2e5c9
Sarah Gilmore <sarahgilmore@sarahs-mbp-2.devices.brown.edu>
2017-05-05 13:45:56 -0400

    working on game menu style

--- src/main/resources/static/js/matches.js
+++ src/main/resources/static/js/matches.js
@@ -852,19 +852,8 @@ function removeBlink(element) {
 
 }
 
-// $name.on(function() {
 
-// })
 
-// function game_name() {
-//   let name = document.getElementById("name").value;
-//   if (name.trim().length != 0) {
-//     document.getElementById("maker").disabled = false;
-//     document.getElementById("maker").removeClass('disabled');
-//   } else {
-//     docu
-//   }
-// }
 
 function loserModal() {
   if (data.loser == myId) {
