48d14b8d57ea4e1259073867e328802f6d6a2eb2
Ben Gabinet <benjamin_gabinet@brown.edu>
2017-05-06 12:43:06 -0400

    Pushing server version.

--- src/main/resources/static/js/matches.js
+++ src/main/resources/static/js/matches.js
@@ -82,8 +82,8 @@ let playerInfo = {};
 // Set up socket connections
 const setup_matches = () => {
 
-  //conn = new WebSocket("ws://107.170.49.223/matches");
-  conn = new WebSocket("ws://localhost:4567/matches");
+  conn = new WebSocket("ws://107.170.49.223/matches");
+  //conn = new WebSocket("ws://localhost:4567/matches");
   conn.onerror = err => {
     console.log('Connection error:', err);
   };
