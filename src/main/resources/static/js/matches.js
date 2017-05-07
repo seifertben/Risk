75db6403877bdac03f6d4947f0d7c8ff8be7c039
Ben Gabinet <benjamin_gabinet@brown.edu>
2017-05-07 13:25:39 -0400

    Merging

--- src/main/resources/static/js/matches.js
+++ src/main/resources/static/js/matches.js
@@ -600,7 +600,7 @@ const setup_matches = () => {
               $("#clickList").show();
               $("#simSel").show();
         	} else {
-              document.getElementById("turn").innerHTML = idToName[data.playerId] + "'s Turn is Reinforcing";
+              document.getElementById("turn").innerHTML = idToName[data.playerId] + "'s to Place Reinforcements";
               addBlink($("#turn"));
               setTimeout(function() {
                 removeBlink($("#turn")); 
